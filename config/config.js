b7dbc94a0f14f79767e686494b9341736891f017
reykz <nasoki.0802@gmail.com>
2026-07-18 13:24:38 -0300

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    botName: "natsukashi",
+    prefix: "!",
+    donos: ["5538991933523"], // Coloque seu número com código do país e DDD
+    emojis: {
+        sucesso: "✅",
+        erro: "❌",
+        carregando: "⏳"
+    }
+};
